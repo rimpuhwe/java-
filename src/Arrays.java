59620e3f12dd1b0b58ed97784a938481fb585f45
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-03-28 08:55:59 +0200

    displaying all items in fruits array

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args){
+        //declaring the array
+        String[] fruits = {"pineapple", "banana","apple", "kiwi"};
+        // displaying all element in fruits array(it's the same as the foreach method  in javascript)
+        for(String fruit : fruits){
+            System.out.println(fruit);
+
+        }
+    }
+
+}
